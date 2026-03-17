4d6cbafc81026a87f966f850a12aaa38ce3ca447
pingz8888 <hills.02-gives@icloud.com>
2026-03-17 22:34:23 +0800

    Update getStocks.js

--- netlify/functions/getStocks.js
+++ netlify/functions/getStocks.js
@@ -1,6 +1,11 @@
 const yahooFinance = require('yahoo-finance2').default;
 
 exports.handler = async function(event, context) {
+    const APP_PASSWORD = process.env.APP_PASSWORD;
+    if (!event.headers['x-app-password'] || event.headers['x-app-password'] !== APP_PASSWORD) {
+        return { statusCode: 401, body: JSON.stringify({ error: "Unauthorized" }) };
+    }
+
     const symbolsParam = event.queryStringParameters.symbols;
     if (!symbolsParam) return { statusCode: 200, body: JSON.stringify({}) };
 
